1c14b887f506b6c6505aaaccad0f0e4963d98b4f
Alex Fourmy <alexfourmy@gmail.com>
2021-07-29 15:10:16 +0200

    Refactored the code to let mongoDB track the user sessions

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 const dotenv = require('dotenv')
 dotenv.config()
-
 const MongoClient = require('mongodb').MongoClient
 
 MongoClient.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
-    module.exports = client.db()
+    module.exports = client
     const app = require('./app')
     app.listen(process.env.PORT)
 })
