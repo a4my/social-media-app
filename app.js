a98289c8e759f5116539ba4c8e1bf939adfad50e
Alex Fourmy <alexfourmy@gmail.com>
2021-08-06 09:26:03 +0200

    made all error and success flash messages available to all templates

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(sessionOptions)
 app.use(flash())
 
 app.use(function(req, res, next){
+  // make all error and succes messages available to all templates
+  res.locals.errors = req.flash("errors")
+  res.locals.success = req.flash("success")
   // make current user id available on the req object
   if(req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
   // make user session data available from within view templates
